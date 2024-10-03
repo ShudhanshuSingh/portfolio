8dfa71d10b55e322ce54fcacf451d36cdaa40373
shudhanshu.blc <shudhanshu.blackcurrant@gmail.com>
2024-10-04 00:28:52 +0530

    fix: footer text

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 export function Footer() {
   return (
@@ -6,7 +6,8 @@ export function Footer() {
       <div className="w-full flex flex-col items-center justify-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by Shudhanshu Singh. The source code is available on{" "}
+            Built by Shudhanshu Singh.
+            {/* The source code is available on{" "}
             <Link
               href="#"
               target="_blank"
@@ -14,8 +15,7 @@ export function Footer() {
               className="font-medium underline underline-offset-4"
             >
               GitHub
-            </Link>
-            .
+            </Link> */}
           </p>
         </div>
       </div>
